74cd88832b11faefb907ec42a844f8dd3c7dbba3
Королев Иван <korolev.ii@phystech.edu>
2025-04-07 10:57:42 +0300

    log

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
 
 typedef unsigned char byte;
 typedef unsigned short int word;
@@ -9,6 +10,65 @@ typedef word address;
 #define MEMSIZE 64*1024
 byte mem[MEMSIZE];
 
+typedef enum {
+    ERROR,
+    WARNING,
+    INFO,
+    TRACE,
+    DEBUG
+} LogLevel;
+
+static LogLevel log_level = INFO;
+
+LogLevel set_log_level(LogLevel level) {
+    LogLevel old_log_level = log_level;
+    log_level = level;
+    return old_log_level;
+}
+
+void log(LogLevel level, const char *format, ...) {
+    if (level <= log_level) {
+        va_list args;
+        va_start(args, format);
+        vprintf(format, args);
+        va_end(args);
+    }
+}
+
+void error(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+    log(ERROR, format, args);
+    va_end(args);
+}
+
+void warning(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+    log(WARNING, format, args);
+    va_end(args);
+}
+
+void info(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+    log(INFO, format, args);
+    va_end(args);
+}
+
+void trace(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+    log(TRACE, format, args);
+    va_end(args);
+}
+
+void debug(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+    log(DEBUG, format, args);
+    va_end(args);
+}
 
 void b_write (address adr, byte val){
     mem[adr] = val;
@@ -139,7 +199,8 @@ int main(int argc, char *argv[])
     mem_dump(0x40, 20);
     printf("\n");
     mem_dump(0x200, 0x26);
-    
+
     fclose(file);
+
     return 0;
 }
